8fee890846f6f603b395e2117e83d2387265c1b3
Evan Miller <evanmillerny@gmail.com>
2017-04-05 14:43:05 -0600

    add uparrow functionality

--- scripts.js
+++ scripts.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
 	}
 });
 
-
 // Clear input fields on click
 $('#input-title').on('click', function() {
 	$('#input-title').val(" ")
@@ -21,7 +20,6 @@ $('#input-title').on('click', function() {
 $('#input-body').on('click', function() {
 	$('#input-body').val(" ")
 })
-
 
 //constructor function
 function Idea(title, body) {
@@ -41,47 +39,72 @@ $(".button-save").on("click", function() {
 	sendToStorage(idea);
 })
 
-$("#new-idea-article").on("click", "#quality", function() {
-	var identifier = $(this).parent().parent().prop('id');
-  console.log(identifier)
-  var qualities = ["swill", "plausible", "genius"];
-  var index = 1;
-  $(this).replaceWith(qualities[index])
+$("#new-idea-article").on("click", ".upvote-image", function() {
+	id = $(this).parent().parent().prop('id');
+	console.log("click id " + id)
+	var parsedObject = JSON.parse(localStorage.getItem(id))
+	var objectQuality = parsedObject.quality
 
-  console.log(	$(this).remove("#quality"))
-
-  // $("#quality").prepend("<h3>"+qualities[index]+"</h3>")
-
-  console.log(qualities[index])
+	if (objectQuality == "swill") {
+		parsedObject.quality = "plausible"
+		$(this).siblings().last().text("plausible")
+		var update = localStorage.setItem(id, JSON.stringify(parsedObject))
+	} else if (objectQuality == "plausible") {
+		parsedObject.quality = "genius"
+		$(this).siblings().last().text("genius")
+		var update = localStorage.setItem(id, JSON.stringify(parsedObject))
+		console.log(update)
+	}
 })
 
+$("#new-idea-article").on("click", ".downvote-image", function() {
+	var id = $(this).parent().parent().prop('id');
+	var parsedObject = JSON.parse(localStorage.getItem(id))
+	var objectQuality = parsedObject.quality
+
+	if (objectQuality == "genius") {
+		parsedObject.quality = "plausible"
+		// $(this).siblings().last().remove()
+
+		$(this).siblings().last().text("plausible")
+		var update = localStorage.setItem(id, JSON.stringify(parsedObject))
+		console.log(update)
+
+	} else if (objectQuality == "plausible") {
+		parsedObject.quality = "swill"
+		// $(this).siblings().last().remove()
+		$(this).siblings().last().text("swill")
+		var update = localStorage.setItem(id, JSON.stringify(parsedObject))
+		console.log(update)
+	}
+
+})
 
 // trashcan delete
 $("#new-idea-article").on('click', '.delete-image', function() {
 	console.log("remove button check");
 	console.log($('.delete-image').parent().parent());
 	//debugger;
-	console.log('ID TO DELETE '+$(this).parent().parent().prop('id'));
+	console.log('ID TO DELETE ' + $(this).parent().parent().prop('id'));
 
 	localStorage.removeItem($(this).parent().parent().prop('id'));
 	$(this).parent().parent().remove('.new-idea-article');
 });
 
-
 function prepend(idea) {
 	console.log(idea.id);
 	$("#new-idea-article").prepend(`
     <div id="${idea.id}" class="new-idea-article">
-    <section>
-    <input type="text" class='new-idea-header' value='${idea.title}' maxlength="30" size="35">
-    <img class="delete-image" src="assets/delete.svg" alt="">
-    </section>
-      <input type="text" class='new-idea-body' value="${idea.body}" maxlenghth="200" size="225">
-    <section class="new-idea-footer">
-    <img class="upvote-image" src="assets/upvote.svg" alt="">
-    <img class="downvote-image" src="assets/downvote.svg" alt="">
-    <h3 class="h3-footer">quality:</h3><h3 id="quality">${idea.quality}</h3>
-    </section>
+	    <section>
+	    	<input type="text" class='new-idea-header' value='${idea.title}' maxlength="30" size="35">
+	    	<button id='delete-image' class="delete-image" type="button" name="button"></button>
+	    </section>
+	      <input type="text" class='new-idea-body' value="${idea.body}" maxlenghth="200" size="225">
+	    <section class="new-idea-footer">
+				<button id="upvote-image" class="upvote-image" type="button" name="button"></button>
+				<button class="downvote-image" type="button" name="button"></button>
+	    	<h3 class="h3-footer">quality:</h3><h3 id="quality">${idea.quality}</h3>
+	    </section>
     </div>
     `);
 	$('#input-title').val("Title")
